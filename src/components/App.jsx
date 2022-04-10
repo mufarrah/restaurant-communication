3b7a9df1bf31863037726cbdfeb9ac0c652a73fa
unknown <a.mufarrah@gmail.com>
2022-04-11 01:34:58 +0300

    front-end-bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+const axios = require('axios');
 
-function App() {
+
+const App = ()=> {
+
+    const makeCall = ()=> {
+        axios({
+            method: 'post',
+            url: 'https://vr-team-api-nodejs.herokuapp.com/makecall',
+            // data: {
+            //   firstName: 'Fred',
+            //   lastName: 'Flintstone'
+            // }
+          }).then (function (response) {console.log(response)
+             });
+    }
   return (
     <div className="container">
-    <img class="image" src="https://i.postimg.cc/VLj8s1fT/COi-White-Transparent-BG.png" alt="Carbon Origins Logo" width="90" height="90" ></img>
+    <img className="image" src="https://i.postimg.cc/VLj8s1fT/COi-White-Transparent-BG.png" alt="Carbon Origins Logo" width="90" height="90" ></img>
      
       <h1>Hello</h1>
       <br></br>
       <h3>Please fill in the form to make a restaurant call</h3>
       <form className="form">
-        <input type="tel" id="phone" name="phone" placeholder="Restaurant phone number"pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-        required></input>
+        <input type="tel" id="phone" name="phone" placeholder="Restaurant phone number"
+        required ></input>
         
         <input type="text" placeholder="Voice call message" required />
-        <button type="submit">Make a call</button>
+        <button type="submit"  onClick={makeCall()} >Make a call</button>
       </form>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+// --
+
+// import * as ReactDOMClient from 'react-dom/client';
+// import App from "./components/App";
+// import React from "react";
+
+// const container = document.getElementById('root');
+// const root = ReactDOMClient.hydrateRoot(container, <root tab="home" />);
+
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./components/App";
+const container = document.getElementById('root');
+// Create a root.
+const root = ReactDOM.createRoot(container);
+// Initial render: Render an element to the root.
+root.render(<App tab="home" />);
+// During an update, there's no need to pass the container again.
+root.render(<App tab="profile" />);
+
+
+
+
+
